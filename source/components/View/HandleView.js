996f163d9726536d37fbeac47cf434ff7ff0f0a1
Roman Yarlykov <yarlykov92@gmail.com>
2021-01-06 21:23:17 +0700

    add: new methods for slider handle and private method "_setup"
    - handlerListener
    - onMouseMove
    - onMouseUp
    -disableDragStart

--- source/components/View/HandleView.js
+++ source/components/View/HandleView.js
@@ -3,15 +3,63 @@ import { inherits } from "util";
 class Handle {
   constructor(node) {
     this.el = node;
-    this.init();
+    this._init();
+    this._setup();
   }
 
-  init() {
-    this.position('left', 40)
+  _init() {
+    this.$handle = this.el.querySelector('#handle')
+    this.$slider = this.el.querySelector('.slider__scale')
   }
+
+  _setup() {
+    this.handleListener = this.handleListener.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+    this.onMouseUp = this.onMouseUp.bind(this)
+    this.disableDragStart = this.disableDragStart.bind(this)
+
+    this.$handle.addEventListener('mousedown', this.handleListener)
+  }
+
+  disableDragStart() {
+    return false;
+  }
+
+  handleListener(event) {
+    event.preventDefault();
+
+    this.shiftX = event.clientX - this.$handle.getBoundingClientRect().left;
+
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+
+    this.$handle.addEventListener('dragstart', this.disableDragStart)
+  }
+
+  onMouseMove(event) {
+    let newLeftPosition = event.clientX - this.shiftX - this.$slider.getBoundingClientRect().left;
+
+    if (newLeftPosition < 0) {
+      newLeftPosition = 0;
+    }
+
+    let rightEdgePosition = this.$slider.offsetWidth - this.$handle.offsetWidth;
+    if (newLeftPosition > rightEdgePosition) {
+      newLeftPosition = rightEdgePosition;
+    }
+
+    this.$handle.style.left = newLeftPosition + 'px';
+  }
+
+  onMouseUp() {
+    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener('mousemove', this.onMouseMove);
+  } 
 
   position(position='left', percent) {
-    this.$handle = this.el.querySelector('#handle')
+    this.$handle = this.el.querySelector('.slider__handle')
+    this.$slider = this.el.querySelector('.slider__scale')
+
 
     this.$handle.style = `${position}: ${percent}%`;
     this.tooltip(percent);
