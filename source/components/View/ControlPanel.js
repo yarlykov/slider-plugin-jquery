79c2286a6a2cb56536b7ceabd0fa6d85a3b1528f
Roman Yarlykov <yarlykov92@gmail.com>
2021-01-19 11:58:31 +0700

    refactor: ContrlPanel class

--- source/components/View/ControlPanel.js
+++ source/components/View/ControlPanel.js
@@ -1,19 +1,47 @@
 class ControlPanel {
-  constructor(node) {
+  constructor(node = []) {
     this.$el = node;
 
     this._init();
-    this.record();
+    this._setup();
   }
 
   _init() {
-    this.$currentSliderValue = this.$el.querySelector('.control-panel__input');
+    this.$mainControl = document.querySelector('.control-panel');
+    this.$controls = this.$mainControl.querySelectorAll('.control-panel__input');
+
+    const [scaleMin, scaleMax, current, step] = this.$controls;
+    this.$current = current;
+
+    this.$currentSliderValue = document.querySelector('.control-panel__input');
   }
 
-  record(sliderValue) {
-    this.$currentSliderValue.value = sliderValue || 7;
+  _setup() {
+    this.bindEventListeners();
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.$mainControl.addEventListener('input', this.handleControlChange);
+  }
+
+  bindEventListeners() {
+    this.handleControlChange = this.handleControlChange.bind(this);
+  }
+
+  handleControlChange(inputEvent) {
+    const { title } = inputEvent.target.dataset;
+
+    if (title === 'current') {
+      console.log('i am work');
+    }
+  }
+
+  setCurrentValue(sliderCurrentValue) {
+    this.$current.value = sliderCurrentValue;
   }
 }
+export default ControlPanel;
 
 const mainNode = document.querySelector('.control-panel');
 
