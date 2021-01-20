fd5f262fabff2d49ee33143eff89b77ad3085bdf
Roman Yarlykov <yarlykov92@gmail.com>
2021-01-20 15:47:35 +0700

    add: render control-panel and slider in Slider class

--- source/components/expComponents/slider/Slider.js
+++ source/components/expComponents/slider/Slider.js
@@ -1,3 +1,6 @@
+import $ from '../../../core/dom';
+import ControlPanel from '../control-panel/ControlPanel';
+
 class Slider {
   constructor(selector, options) {
     this.init(selector);
@@ -5,7 +8,50 @@ class Slider {
   }
 
   init(mainHtmlNode) {
-    this.$mainHtmlNode = document.querySelector(mainHtmlNode);
+    this.$mainHtmlNode = $(mainHtmlNode);
+  }
+
+  toHTML() {
+    return `
+      <div class="slider slider_horizontal" id="slider">
+        <div class="slider__scale slider__scale_horizontal">
+          <div class="slider__fill slider__fill_horizontal slider__fill_orange" id="fill"></div>
+          <div class="slider__lever slider__lever_orange slider__lever_horizontal" id='lever'>
+            <div class="slider__tooltip slider__tooltip_horizontal slider__tooltip_orange">
+              <span class="tooltip__value">53 ¥</span>
+              <div class="slider__tooltip_arrow"></div>
+            </div>
+          </div>
+          <div class="slider__labels slider__labels_horizontal">
+            <div class="slider__labels-item">0</div>
+            <div class="slider__labels-item">25</div>
+            <div class="slider__labels-item">50</div>
+            <div class="slider__labels-item">75</div>
+            <div class="slider__labels-item">100</div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getRoot() {
+    const $root = $.create('div', 'demo-page__block');
+    const controlPanel = new ControlPanel();
+    const $controlPanel = $.create('div', ControlPanel.className);
+    $controlPanel.html(controlPanel.toHTML());
+
+    const $slider = $.create('div', 'slider');
+    $slider.$nativeElement.classList.add('slider_horizontal');
+    $slider.html(this.toHTML());
+
+    $root.append($controlPanel);
+    $root.append($slider);
+
+    return $root;
+  }
+
+  render() {
+    this.$mainHtmlNode.append(this.getRoot());
   }
 }
 
--- source/index.js
+++ source/index.js
@@ -2,15 +2,16 @@
 // import './components/View/ControlPanel';
 
 import Slider from './components/expComponents/slider/Slider';
-import ControlPanel from './components/expComponents/control-panel/ControlPanel';
-import Lever from './components/expComponents/lever/Lever';
-import Tooltip from './components/expComponents/tooltip/Tooltip';
-import Fill from './components/expComponents/fill/Fill';
-import Labels from './components/expComponents/labels/Labels';
+// import ControlPanel from './components/expComponents/control-panel/ControlPanel';
+// import Scale from './components/expComponents/scale/Scale';
+// import Lever from './components/expComponents/lever/Lever';
+// import Tooltip from './components/expComponents/tooltip/Tooltip';
+// import Fill from './components/expComponents/fill/Fill';
+// import Labels from './components/expComponents/labels/Labels';
 import './demo-page/styles/main.scss';
 
-const slider = new Slider('#slider', {
-  components: [ControlPanel, Lever, Tooltip, Fill, Labels],
+const slider = new Slider('.demo-page__wrapper', {
+  components: [],
 });
 
-console.log(slider);
+slider.render();
