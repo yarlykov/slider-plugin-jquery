aa85670e5b4a859361b9ac367036c905de4d3c51
Roman Yarlykov <yarlykov92@gmail.com>
2021-01-20 15:44:37 +0700

    add: Scale class

---
+++ source/components/expComponents/scale/Scale.js
@@ -0,0 +1,10 @@
+import SliderComponent from '../../../core/SliderComponent';
+
+class Scale extends SliderComponent {
+  toHTML() {
+    return '<h1>Scale</h1>';
+  }
+}
+Scale.className = 'slider__scale';
+
+export default Scale;
