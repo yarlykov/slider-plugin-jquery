1bab0cdf60a7db892cf6370d7e8d600d8fcbcfc9
Roman Yarlykov <yarlykov92@gmail.com>
2021-01-20 15:43:43 +0700

    add: methods
    - html()
    - clear()
    - append()

--- source/core/dom.js
+++ source/core/dom.js
@@ -1,14 +1,45 @@
 class Dom {
+  constructor(selector) {
+    this.$nativeElement = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector;
+  }
+ 
+  html(html) {
+    if (typeof html === 'string') {
+      this.$nativeElement.innerHTML = html;
+      return this;
+    }
+    return this.$nativeElement.outerHTML.trim();
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  append(node) {
+    let domNode = node;
+    if (domNode instanceof Dom) {
+      domNode = domNode.$nativeElement;
+    }
+    if (Element.prototype.append) {
+      this.$nativeElement.append(domNode);
+    } else {
+      this.$nativeElement.appendChild(domNode);
+    }
+    return this;
+  }
 }
 
-export default function $() {
-  return new Dom();
+export default function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
-  const el = document.createElement(tagName);
+  const nativeElement = document.createElement(tagName);
   if (classes) {
-    el.classList.add(classes);
+    nativeElement.classList.add(classes);
   }
-  return el;
-}
+  return $(nativeElement);
+};
