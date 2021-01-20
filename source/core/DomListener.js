e3c417d6881b50d3c549f0ba4de64528722bf48f
Roman Yarlykov <yarlykov92@gmail.com>
2021-01-20 15:46:53 +0700

    add: constructor

--- source/core/DomListener.js
+++ source/core/DomListener.js
@@ -1,5 +1,14 @@
 class DomListener {
+  constructor($root) {
+    // this.root($root);
+  }
 
+  root($root) {
+    if (!$root) {
+      throw new Error('No $root provided for DomListener!');
+    }
+    this.$root = $root;
+  }
 }
 
 export default DomListener;
