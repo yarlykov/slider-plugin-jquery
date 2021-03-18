d6889c3e6d68e3faa5964a6bbff160ace3fc4590
Roman Yarlykov <yarlykov92@gmail.com>
2021-03-18 21:30:45 +0700

    add: slider version-3.0

--- source/index.js
+++ source/index.js
@@ -1,41 +1,7 @@
-// import './components/View/LeverView';
-// import './components/View/ControlPanel';
-
-import Container from './components/Container/Container';
-import ControlPanel from './components/control-panel/ControlPanel';
-import Slider from './components/slider/Slider';
-import Fill from './components/fill/Fill';
-import Labels from './components/labels/Labels';
-import Lever from './components/lever/Lever';
-import Scale from './components/scale/Scale';
-import Tooltip from './components/tooltip/Tooltip';
-
-import rootReducer from './redux/rootReducer';
-import CreateStore from './core/CreateStore';
-
+import View from './components/View';
 import './demo-page/styles/main.scss';
-import { storage } from './core/utils';
-import initialState from './redux/initialState';
 
-// const wrapper = document.querySelectorAll('.demo-page__block');
+const slider = document.querySelector('.slider-plugin');
+const view = new View(slider);
 
-// wrapper.forEach((node) => {
-//   console.log(node);
-//   const container = new Container(node, {
-//     components: [ControlPanel, Slider, Fill, Labels, Lever, Scale, Tooltip],
-//   });
-//   container.render();
-// });
-
-const store = new CreateStore(rootReducer, initialState);
-
-store.subscribe((state) => {
-  storage('slider-state', state);
-});
-
-const container = new Container('.demo-page__block', {
-  components: [ControlPanel, Slider, Fill, Labels, Lever, Scale, Tooltip],
-  store,
-});
-
-container.getRoot();
+view.render();
--- source/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-function rootReducer(state, action) {
-  switch (action.type) {
-    case 'SLIDER_POSITION_CHANGE':
-      return { ...state, sliderPositionState: action.data };
-    default: return state;
-  }
-}
-
-export default rootReducer;
