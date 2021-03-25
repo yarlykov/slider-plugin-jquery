092d5c0c9b8b8f4db3950e06ecca5f707d4f941a
Roman Yarlykov <yarlykov92@gmail.com>
2021-03-25 19:35:02 +0700

    add: function bemName

---
+++ source/bemName.js
@@ -0,0 +1,28 @@
+const separators = { element: '__', modifier: '_' };
+
+function isString(string) {
+  if (typeof (string) === 'string') {
+    return string;
+  }
+  return '';
+}
+
+function bemNames(names = {}) {
+  const { b = '', e = '', m = '' } = names;
+  const haveAll = (isString(b) && isString(e) && isString(m)) !== '';
+  const blockAndElement = (isString(b) && isString(e)) !== '';
+  const blockAndModifier = (isString(b) && isString(m)) !== '';
+
+  if (haveAll) {
+    return `${b}${separators.element}${e}${separators.modifier}${m}`;
+  }
+  if (blockAndElement) {
+    return `${b}${separators.element}${e}`;
+  }
+  if (blockAndModifier) {
+    return `${b}${separators.modifier}${m}`;
+  }
+  return `${b}`;
+}
+
+export default bemNames;
