b88f9d2780fa12efda55b2f3b998149d1c27e7a8
Roman Yarlykov <yarlykov92@gmail.com>
2021-04-21 18:13:36 +0700

    add: new test in wrapper class

--- source/components/View/subViews/Wrapper/wrapper.test.ts
+++ source/components/View/subViews/Wrapper/wrapper.test.ts
@@ -1,9 +1,21 @@
 import Wrapper from './Wrapper';
 
 describe('Wrapper:create ', () => {
+
+  let wrapper: any;
+  let root: HTMLElement;
+
+  beforeEach(() => {
+    wrapper = new Wrapper();
+    root = document.createElement('div');
+  })
+
+
   test('create wrapper HTMLElement', () => {
-    const wrapper = new Wrapper();
-    const root = document.createElement('div');
     expect(wrapper.create({}, root)).toBeInstanceOf(HTMLElement);
+  });
+
+  test('create child HTMLElement in wrapper', () => {
+    expect(wrapper.create({}, root).childNodes).toHaveLength(1);
   });
 });
