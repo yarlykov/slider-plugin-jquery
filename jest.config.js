aade753a9b8fd31f4dd90bc4d896b66da02bc6b8
Roman Yarlykov <yarlykov92@gmail.com>
2021-06-24 21:08:52 +0700

    edit: configs
    - jest
    - eslint

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
-  // collectCoverageFrom: [
-  //   '<rootDir>/source/**/{!(),}.ts',
-  // ],
+  collectCoverageFrom: [
+    '<rootDir>/source/**/{!(Presenter|interfaces),}.ts',
+  ],
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/test/jest/__mocks__/styleMock.ts',
   },
