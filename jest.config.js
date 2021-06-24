2f67463092e5c7a0e8d234dfdb67e751a7ae4ed6
Roman Yarlykov <yarlykov92@gmail.com>
2021-06-24 14:55:51 +0700

    add: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
+  // collectCoverageFrom: [
+  //   '<rootDir>/source/**/{!(),}.ts',
+  // ],
   moduleNameMapper: {
-    '\\.(css|scss)$': '<rootDir>/test/jest/__mocks__/styleMock.js',
+    '\\.(css|scss)$': '<rootDir>/test/jest/__mocks__/styleMock.ts',
   },
 };
