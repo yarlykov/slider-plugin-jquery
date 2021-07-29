4b3dfbff3a529330d44820cc8a43a223d773f530
Roman Yarlykov <yarlykov92@gmail.com>
2021-07-29 22:49:29 +0700

    change: initial values

--- demo-page/index.ts
+++ demo-page/index.ts
@@ -3,17 +3,19 @@ import DemoBlock from './DemoBlock';
 
 const slider = $('#sliderSingleHorizontal');
 slider.sliderPlugin({
-  valueFrom: 50,
-  min: -10,
-  max: 300,
-  step: 25,
+  valueFrom:0,
+  min: -100,
+  max: 100,
+  step: 50,
 });
 const sliderSingleHorizontal = new DemoBlock(slider);
 sliderSingleHorizontal.init();
 
 const vertical = $('#sliderSingleVertical');
 vertical.sliderPlugin({
-  max: 110,
+  max: 77,
+  valueFrom: 42,
+  step: 3,
   orientation: 'vertical',
 });
 const sliderSingleVertical = new DemoBlock(vertical);
@@ -22,7 +24,8 @@ sliderSingleVertical.init();
 
 const range = $('#sliderRangeHorizontal');
 range.sliderPlugin({
-  valueTo: 34,
+  valueFrom: 23,
+  valueTo: 77,
   orientation: 'horizontal',
   range: true,
   color: 'green',
@@ -32,9 +35,12 @@ sliderRangeHorizontal.init();
 
 const rangeVertical = $('#sliderRangeVertical');
 rangeVertical.sliderPlugin({
+  min: -200000,
+  max: 200000,
   range: true,
-  valueTo: 62,
-  valueFrom: 78,
+  valueTo: 120000,
+  valueFrom: -120000,
+  step: 1,
   orientation: 'vertical',
   color: 'green',
 });
