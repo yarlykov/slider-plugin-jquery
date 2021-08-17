a9cb2165a939c39230a59d16969f2c1c62f04f37
Roman Yarlykov <yarlykov92@gmail.com>
2021-08-17 21:23:35 +0700

    refactor: DemoBlock

--- demo-page/DemoBlock.ts
+++ demo-page/DemoBlock.ts
@@ -32,7 +32,13 @@ class DemoBlock {
     this.root = root;
   }
 
-  init(): void {
+  public init(): void {
+    this.findDOMElements();
+    this.pluginSetup();
+    this.bindEventListeners();
+  }
+
+  private findDOMElements() {
     this.panel = this.root
       .get(0)
       .parentElement?.querySelector('[data-id="control-panel"]') as HTMLElement;
@@ -67,7 +73,9 @@ class DemoBlock {
     this.tooltips = this.panel.querySelector(
       '[data-title="tooltips"]',
     ) as HTMLInputElement;
+  }
 
+  private pluginSetup() {
     this.state = this.root.sliderPlugin('getState');
     this.min.value = `${this.state.min}`;
     this.max.value = `${this.state.max}`;
@@ -84,115 +92,145 @@ class DemoBlock {
     this.range.checked = this.state.range as boolean;
     this.labels.checked = this.state.labels as boolean;
     this.tooltips.checked = this.state.tooltips as boolean;
-
-    this.root.sliderPlugin('onChange', (event: CustomEvent) => {
-      this.state = event.detail;
-      if (this.state.range) {
-        this.valueTo.disabled = false;
-        this.valueTo.value = event.detail.valueTo;
-      } else {
-        this.valueTo.disabled = true;
-      }
-      this.valueFrom.value = event.detail.valueFrom;
-      this.step.value = event.detail.step;
-      this.min.value = event.detail.min;
-      this.max.value = event.detail.max;
-      this.orientation.value = event.detail.orientation;
-      this.fill.checked = event.detail.fill;
-      this.range.checked = event.detail.range;
-      this.labels.checked = event.detail.labels;
-      this.tooltips.checked = event.detail.tooltips;
-    });
-
-    this.addEventListeners();
   }
 
-  addEventListeners() {
-    this.valueFrom.addEventListener('change', () => {
-      let value = 0;
-      value = Number(this.valueFrom.value);
-      this.root.sliderPlugin('setValue', 'valueFrom', value);
-    });
+  private bindEventListeners() {
+    this.root.sliderPlugin('onChange', this.handleOnChangeRoot.bind(this));
+    this.valueFrom.addEventListener(
+      'change',
+      this.handleValueFromChange.bind(this),
+    );
+    this.valueFrom.addEventListener(
+      'keydown',
+      this.handleValueFromKeydown.bind(this),
+    );
+    this.valueTo.addEventListener(
+      'change',
+      this.handleValueToChange.bind(this),
+    );
+    this.valueTo.addEventListener(
+      'keydown',
+      this.handleValueToKeydown.bind(this),
+    );
+    this.step.addEventListener('change', this.handleStepChange.bind(this));
+    this.min.addEventListener('change', this.handleMinChange.bind(this));
+    this.max.addEventListener('change', this.handleMaxChange.bind(this));
+    this.orientation.addEventListener(
+      'change',
+      this.handleOrientationChange.bind(this),
+    );
+    this.fill.addEventListener('change', this.handleFillChange.bind(this));
+    this.range.addEventListener('change', this.handleRangeChange.bind(this));
+    this.labels.addEventListener('change', this.handleLabelsChange.bind(this));
+    this.tooltips.addEventListener(
+      'change',
+      this.handleTooltipsChange.bind(this),
+    );
+  }
 
-    this.valueFrom.addEventListener('keydown', (event: KeyboardEvent) => {
-      const { valueFrom = 0, step = 0 } = this.state;
-      const { code } = event;
+  private handleOnChangeRoot(event: CustomEvent) {
+    this.state = event.detail;
+    if (this.state.range) {
+      this.valueTo.disabled = false;
+      this.valueTo.value = event.detail.valueTo;
+    } else {
+      this.valueTo.disabled = true;
+    }
+    this.valueFrom.value = event.detail.valueFrom;
+    this.step.value = event.detail.step;
+    this.min.value = event.detail.min;
+    this.max.value = event.detail.max;
+    this.orientation.value = event.detail.orientation;
+    this.fill.checked = event.detail.fill;
+    this.range.checked = event.detail.range;
+    this.labels.checked = event.detail.labels;
+    this.tooltips.checked = event.detail.tooltips;
+  }
 
-      let newValue = 0;
-      if (code === 'ArrowUp' || code === 'ArrowRight') {
-        newValue = valueFrom + step;
-        this.root.sliderPlugin('setValue', 'valueFrom', newValue);
-      }
-      if (code === 'ArrowDown' || code === 'ArrowLeft') {
-        newValue = valueFrom - step;
-        this.root.sliderPlugin('setValue', 'valueFrom', newValue);
-      }
-    });
+  private handleValueFromChange() {
+    let value = 0;
+    value = Number(this.valueFrom.value);
+    this.root.sliderPlugin('setValue', 'valueFrom', value);
+  }
 
-    this.valueTo.addEventListener('keydown', (event: KeyboardEvent) => {
-      const { valueTo = 0, step = 0 } = this.state;
-      const { code } = event;
+  private handleValueFromKeydown(event: KeyboardEvent) {
+    const { valueFrom = 0, step = 0 } = this.state;
+    const { code } = event;
 
-      let newValue = 0;
-      if (code === 'ArrowUp' || code === 'ArrowRight') {
-        newValue = valueTo + step;
-        this.root.sliderPlugin('setValue', 'valueTo', newValue);
-      }
-      if (code === 'ArrowDown' || code === 'ArrowLeft') {
-        newValue = valueTo - step;
-        this.root.sliderPlugin('setValue', 'valueTo', newValue);
-      }
-    });
+    let newValue = 0;
+    if (code === 'ArrowUp' || code === 'ArrowRight') {
+      newValue = valueFrom + step;
+      this.root.sliderPlugin('setValue', 'valueFrom', newValue);
+    }
+    if (code === 'ArrowDown' || code === 'ArrowLeft') {
+      newValue = valueFrom - step;
+      this.root.sliderPlugin('setValue', 'valueFrom', newValue);
+    }
+  }
 
-    this.step.addEventListener('change', () => {
-      const value = Number(this.step.value);
-      this.root.sliderPlugin('setValue', 'step', value);
-    });
+  private handleValueToChange() {
+    const value = Number(this.valueTo.value);
+    this.root.sliderPlugin('setValue', 'valueTo', value);
+  }
 
-    this.min.addEventListener('change', () => {
-      const value = Number(this.min.value);
-      this.root.sliderPlugin('setValue', 'min', value);
-    });
+  private handleValueToKeydown(event: KeyboardEvent) {
+    const { valueTo = 0, step = 0 } = this.state;
+    const { code } = event;
 
-    this.max.addEventListener('change', () => {
-      const value = Number(this.max.value);
-      this.root.sliderPlugin('setValue', 'max', value);
-    });
+    let newValue = 0;
+    if (code === 'ArrowUp' || code === 'ArrowRight') {
+      newValue = valueTo + step;
+      this.root.sliderPlugin('setValue', 'valueTo', newValue);
+    }
+    if (code === 'ArrowDown' || code === 'ArrowLeft') {
+      newValue = valueTo - step;
+      this.root.sliderPlugin('setValue', 'valueTo', newValue);
+    }
+  }
 
-    this.valueTo.addEventListener('change', () => {
-      const value = Number(this.valueTo.value);
-      this.root.sliderPlugin('setValue', 'valueTo', value);
-    });
+  private handleStepChange() {
+    const value = Number(this.step.value);
+    this.root.sliderPlugin('setValue', 'step', value);
+  }
 
-    this.orientation.addEventListener('change', () => {
-      const { value } = this.orientation;
-      this.root.sliderPlugin('setValue', 'orientation', value);
-    });
+  private handleMinChange() {
+    const value = Number(this.min.value);
+    this.root.sliderPlugin('setValue', 'min', value);
+  }
 
-    this.fill.addEventListener('change', () => {
-      const value: boolean = this.fill.checked;
-      this.root.sliderPlugin('setValue', 'fill', value);
-    });
+  private handleMaxChange() {
+    const value = Number(this.max.value);
+    this.root.sliderPlugin('setValue', 'max', value);
+  }
 
-    this.range.addEventListener('change', () => {
-      const value: boolean = this.range.checked;
-      this.root.sliderPlugin('setValue', 'range', value);
-      if (value) {
-        this.root.sliderPlugin('setValue', 'color', 'green');
-      } else {
-        this.root.sliderPlugin('setValue', 'color', 'orange');
-      }
-    });
+  private handleOrientationChange() {
+    const { value } = this.orientation;
+    this.root.sliderPlugin('setValue', 'orientation', value);
+  }
 
-    this.labels.addEventListener('change', () => {
-      const value: boolean = this.labels.checked;
-      this.root.sliderPlugin('setValue', 'labels', value);
-    });
+  private handleFillChange() {
+    const value: boolean = this.fill.checked;
+    this.root.sliderPlugin('setValue', 'fill', value);
+  }
 
-    this.tooltips.addEventListener('change', () => {
-      const value: boolean = this.tooltips.checked;
-      this.root.sliderPlugin('setValue', 'tooltips', value);
-    });
+  private handleRangeChange() {
+    const value: boolean = this.range.checked;
+    this.root.sliderPlugin('setValue', 'range', value);
+    if (value) {
+      this.root.sliderPlugin('setValue', 'color', 'green');
+    } else {
+      this.root.sliderPlugin('setValue', 'color', 'orange');
+    }
+  }
+
+  private handleLabelsChange() {
+    const value: boolean = this.labels.checked;
+    this.root.sliderPlugin('setValue', 'labels', value);
+  }
+
+  private handleTooltipsChange() {
+    const value: boolean = this.tooltips.checked;
+    this.root.sliderPlugin('setValue', 'tooltips', value);
   }
 }
 
